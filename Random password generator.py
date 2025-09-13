217824ec6e6633e4756acbf7f767461e02f0d854
ItsTheRealKappa <148846339+ItsTheRealKappa@users.noreply.github.com>
2025-09-13 16:04:54 +1000

    Add files via upload
    ### 🔐 PyPassword Generator
    
    This is a simple Python password generator built using basic concepts from the first few days of the **"100 Days of Code: Python Bootcamp"** by Angela Yu.
    
    It allows the user to specify how many **letters**, **symbols**, and **numbers** they want in their password. The program randomly selects characters from predefined lists, combines them, and outputs a generated password.
    
    ---

---
+++ Random password generator.py
@@ -0,0 +1,29 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+auto_letters = (random.sample(letters,nr_letters))
+auto_symbols = (random.sample(symbols,nr_symbols))
+auto_numbers = (random.sample(numbers,nr_numbers))
+
+word_letters = ""
+for letter in auto_letters:
+    word_letters = word_letters + letter
+
+word_symbols = ""
+for symbol in auto_symbols:
+    word_symbols = word_symbols + symbol
+
+word_numbers = ""
+for numbers in auto_numbers:
+    word_numbers = word_numbers + numbers
+
+password = word_letters+word_symbols+word_numbers
+print(password)
